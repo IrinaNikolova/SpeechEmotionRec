ff712a66c7c1d9416df4ab18c9d925cedd54e391
IrinaNikolova <in1u18@soton.ac.uk>
2021-04-08 01:45:12 +0300

    displaying tips

--- frontend/ser-web/src/components/HelloWorld.vue
+++ frontend/ser-web/src/components/HelloWorld.vue
@@ -3,8 +3,8 @@
     <b-alert variant="danger" :show="showAlert">Please upload .wav file!</b-alert>
     <h1>{{ msg }}</h1>
     <div class="large-12 medium-12 small-12 cell">
-      <label>File
-        <input type="file" accept=".wav" @change="handleFileUpload($event)"/>
+      <label>
+        <b-form-file type="file" accept=".wav" @change="handleFileUpload($event)"/>
       </label>
     </div>
     <div>
@@ -14,28 +14,43 @@
       <b-card
         title="Emotion"
         class="mb-2"
+        border-variant="light"
       >
         <b-card-text>
           The algorithm predicts you are: {{this.emotion}}
         </b-card-text>
 
       </b-card>
+      <b-collapse id="collapse" class="mt-2">
+        <b-card>
+           <ul>
+            <li v-for="tip in tips" :key="tip">
+              {{ tip }}
+            </li>
+           </ul>
+        </b-card>
+      </b-collapse>
     </div>
+    <b-img :src="require('../assets/Speech.jpg')" fluid-grow/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
+
 
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
+  // @Prop({type: Object as () => MyObj})
+  // myObj!: MyObj
   //initalize empty file
   file: File = new File([""], "");
   apiResponse = "";
   showAlert = false;
   emotion = "";
+  tips = [];
 
   handleFileUpload(event: Event): void{
     const target = event.target as HTMLInputElement
@@ -53,9 +68,12 @@ export default class HelloWorld extends Vue {
   async submitFile(): Promise<void>{
     let formData: FormData = new FormData();
     formData.append('file', this.file);
+    // eslint-disable-next-line
     const response = await axios.post(`http://localhost:5000/file`, formData).then((response) => {
-      console.log(response);
-      this.emotion = response.data;
+      console.log(response.data);
+      this.emotion = response.data.emotion;
+      this.tips = response.data.tips.split('\n');
+      this.$root.$emit('bv::toggle::collapse', 'collapse')
     })
   }
 
@@ -79,4 +97,5 @@ li {
 a {
   color: #42b983;
 }
+
 </style>
